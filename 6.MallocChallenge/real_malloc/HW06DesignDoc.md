1748b4beea3b2dea3d7a5f65bd19c42f77e87b3b
Carina Samson <carinasamson@berkeley.edu>
2021-06-19 14:52:44 +0900

    Updated Malloc

--- 6.MallocChallenge/real_malloc/HW06DesignDoc.md
+++ 6.MallocChallenge/real_malloc/HW06DesignDoc.md
@@ -5,107 +5,124 @@
 <u>**Malloc Challenge**</u>
 -------
 *Summary*
+My implementation for this challenge combined both the "best fit" algorithm, as well as "merge." Most of the code was adapted from simple_malloc.c and the changes that I made to it was for the purpose of optimizing efficiency, as well as memory storage.  My scan and merge method in particular improved memory utilization by merging free blocks into one block, looking for the best fit for the smallest available memory slot and avoiding memory fragmentation in the process. The my_add_to_freelist function was also updated to sort the free list by address for faster merging.
 
-My implementation for this challenge combined both the "best fit" algorithm, as well as "scan and merge." Most of the code was adapted from simple_malloc.c and the changes that I made to it was for the purpose of optimizing efficiency, as well as memory storage. My scan and merge method in particular improved memory utilization by merging free blocks into one block, looking for the best fit for the smallest available memory slot and avoiding memory fragmentation in the process.
+*Analysis*
+I compared the results of my improved best fit with merge algorithm, with the first fit, worst fit and naive best fit algorithms and the results are displayed in the table below. The first fit algorithm finds the first free slot where the object can fit. As you can see from the results, the execution time is fast for the challenge 1-3 but it became so slower in challenge 4 and 5. The memory utilization also decreased and it was not efficient because of memory fragmentation. The worst fit algorithm finds the largest slot where the object fits. It was the slowest algorithm as you can see from the results and the memory utilization was also the worst compared to the others. My improved best fit and merge algorithm had better memory utilization compared to other algorithms. The execution time was also fast in challenge 4 and 5. 
 
-*Algorithm (Best Fit)*
 
-1. Traverse the free list and find the best fit slot in the free list 
+*Results*
+| -------------- | ---------------- | ---------------- | ---------------- | --------------- | --------------- |
+| Algorithm      | Challenge 1      | Challenge 2      | Challenge 3      |Challenge 4      |Challenge 5      |
+| -------------- | ---------------- | ---------------- | ---------------- |---------------- |---------------- |
+| Best Fit       | Time: 392 ms     | Time: 110 ms     | Time: 170 ms     | Time: 1916 ms   | Time: 1371 ms   |
+| and merge      | Utilization: 70% | Utilization: 40% | Utilization: 44% | Utilization: 75 | Utilization: 76%|
+| -------------- | ---------------- | ---------------- | ---------------- | --------------- | --------------- |
+| First Fit      | Time: 7 ms       | Time: 6 ms       | Time: 106 ms     | Time: 6644 ms   | Time: 4961 ms   |
+|                | Utilization: 70% | Utilization: 40% | Utilization: 7%  | Utilization: 15%| Utilization: 15%|
+| -------------- | ---------------- | ---------------- | ---------------- | --------------- | --------------- |
+| Worst Fit      | Time: 1233 ms    | Time: 428 ms     | Time: 44751 ms   | Time: 247198 ms | Time: 197912ms  |
+|                | Utilization: 70% | Utilization: 40% | Utilization: 4%  | Utilization: 7% | Utilization: 7% |
+| -------------- | ---------------- | ---------------- | ---------------- | --------------- | --------------- |
+
+
+
+*Algorithm: Best fit with merge*
+1. Traverse the free list and find the best fit slot in the free list
     1. If the metadata size is greater than or equal to the size
     2. Set the best_fit_metadata equal to the metadata
     3. Set the prev_fit_metadata equal to the prev pointer
     4. Find the smallest free slot(best_fit_metadata) that can be allocated
-    5. Repeat until  we've reached the end of the free list 
-     
+    5. Repeat until  we've reached the end of the free list
+
 2. If we're unable to find a memory block on the free list
     1. Request more memory from the system
-    2. Add the new memory  to the free list
+    2. Add the new memory to the free list, sorted by address
     3. Call malloc again
 
-4. After finding the best fit slot 
+4. After finding the best fit slot
     1. Set pointer equal to best_fit_metadata + 1
-    2. Set best_fit_metadata-> size equal to size 
+    2. Set best_fit_metadata-> size equal to size
     3. Remove the memory slot from free list
     4. Create a new metadata from the remaining free slot
-    5. Add the new metadata to the free list
-    6. Return the pointer to the requested memory 
+    5. Add the new metadata to the free list, sorted by address
+    6. Return the pointer to the requested memory
 
 5.  In the my_free implementation
-    1. Add the free slot to the free list
-    2. Scan the free list for continuous free blocks
-    3. Merge the continuous free blocks
-    5. Create a new block with the sum of their sizes
-    
-*Variables*
-- `my_heap` - pointer to the free list
+    1. Add the free slot to the free list, sorted by address
+    2. Scan the free list for continuous free blocks of memory
+    3. Merge the two continuous free blocks
+    4. Create a new block with the sum of their sizes
 
+
+*Data Structures*
+1. my_medata_t
+
+```
+typedef struct my_metadata_t {
+    size_t size;
+    struct my_metadata_t *next;
+} my_metadata_t;
+
+```
+- Each object or free slot has metadata just prior to it:
+... | m | object | m | free slot | m | free slot | m | object | ...
+
+where |m| indicates metadata. The metadata is needed for two purposes:
+- For an allocated object:
+    - |size| indicates the size of the object. |size| does not include the size of the metadata.
+    - |next| pointer set to NULL
+- For a free slot:
+    - |size| indicates the size of the free slot. |size| does not include the size of the metadata.
+    - The free slots are linked with a singly linked list (we call this a free list).
+    - |next| points to the next free slot.`
+
+
+2. my_heap_t
+
+```
+typedef struct my_heap_t {
+    my_metadata_t *free_head;
+    my_metadata_t dummy;
+} my_heap_t;
+```
+- The global information of the my_malloc.
+- |free_head| points to the first free slot.
+- |dummy| is a dummy free slot (only used to make the free list implementation simpler).
+
+
+*Variables*
+- `my_heap_t my_heap` - free list
 
 *Functions*
 
 - `my_remove_from_freelist()`
     - Domain Parameter(s):  metadata, *prev
     - Range: void
-    
+    - Remove a free slot from the free list.
+
 - `my_add_to_freelist()`
     - Domain Parameter(s): *metadata
     - Range: void
-    
+    - Add a free slot to the free list sorted by address for faster merging
+
 - `my_scan_and_merge()`
-    - Domain Parameter(s): 
+    - Domain Parameter(s):
     - Range: void
+    - Scan continuous addressed free blocks. Merge two blocks and create a new block with the sum of their sizes.
 
 - `my_initialize()`
     - Domain Parameter(s):
     - Range: void
+    - This is called only once at the beginning of each challenge.
 
 - `my_malloc()`
     - Domain Parameter(s): size
-    - Range: my_memory_block
- 
+    - Range: *ptr
+    - This is called every time an object is allocated..
+
 - `my_free()`
     - Domain Parameter(s): *ptr
     - Range: void
-
-*Results*
-
-```
-Challenge 1: simple malloc => my malloc
-
-Time: 8 ms => 457 ms
-
-Utilization: 70% => 65%
-
-==================================
-
-Challenge 2: simple malloc => my malloc
-
-Time: 5 ms => 125 ms
-
-Utilization: 40% => 31%
-
-==================================
-
-Challenge 3: simple malloc => my malloc
-
-Time: 100 ms => 166 ms
-
-Utilization: 8% => 37%
-
-==================================
-
-Challenge 4: simple malloc => my malloc
-
-Time: 5846 ms => 1775 ms
-
-Utilization: 15% => 73%
-
-==================================
-
-Challenge 5: simple malloc => my malloc
-
-Time: 4347 ms => 1253 ms
-
-Utilization: 15% => 74%
-
-==================================
-```
+    - Add the free slot to the free list.
+    - Call scan_and_merge to merge two free continuous blocks and create a new block.
--- 6.MallocChallenge/real_malloc/malloc.c
+++ 6.MallocChallenge/real_malloc/malloc.c
@@ -1,23 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+/*                 (๑＞◡＜๑)  Malloc Challenge!!  (◍＞◡＜◍)                   */
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// Welcome to Malloc Challenge!! Your job is to invent a smart malloc algorithm.
+//
+// Rules:
+//
+// 1. Your job is to implement my_malloc(), my_free() and my_initialize().
+//   *  my_initialize() is called only once at the beginning of each challenge.
+//      You can initialize the memory allocator.
+//   *  my_malloc(size) is called every time an object is allocated. In this
+//      challenge, |size| is guaranteed to be a multiple of 8 bytes and meets
+//      8 <= size <= 4000.
+//   * my_free(ptr) is called every time an object is freed.
+//   * Additionally, my_finalize() is called only once at the end of each
+//   challenge,
+//     so you can use this function for doing some clean ups if you want.
+// 2. The only library functions you can use in my_malloc() and my_free() are
+//    mmap_from_system() and munmap_to_system().
+//   *  mmap_from_system(size) allocates |size| bytes from the system. |size|
+//      needs to be a multiple of 4096 bytes. mmap_from_system(size) is a
+//      system call and heavy. You are expected to minimize the call of
+//      mmap_from_system(size) by reusing the returned
+//      memory region as much as possible.
+//   *  munmap_to_system(ptr, size) frees the memory region [ptr, ptr + size)
+//      to the system. |ptr| and |size| need to be a multiple of 4096 bytes.
+//      You are expected to free memory regions that are unused.
+//   *  You are NOT allowed to use any other library functions at all, including
+//      the default malloc() / free(), std:: libraries etc. This is because you
+//      are implementing malloc itself -- if you use something that may use
+//      malloc internally, it will result in an infinite recurion.
+// 3. simple_malloc(), simple_free() and simple_initialize() in simple_malloc.c
+//    are an example of straightforward implementation.
+//    Your job is to invent a smarter malloc algorithm than the simple malloc.
+// 4. There are five challenges (Challenge 1, 2, 3, 4 and 5). Each challenge
+//    allocates and frees many objects with different patterns. Your malloc
+//    is evaluated by two criteria.
+//   *  [Speed] How faster your malloc finishes the challange compared to
+//      the simple malloc.
+//   *  [Memory utilization] How much your malloc is memory efficient.
+//      This is defined as (S1 / S2), where S1 is the total size of objects
+//      allocated at the end of the challange and S2 is the total size of
+//      mmap_from_system()ed regions at the end of the challenge. You can
+//      improve the memory utilization by decreasing memory fragmentation and
+//      reclaiming unused memory regions to the system with munmap_to_system().
+// 5. This program works on Linux and Mac but not on Windows. If you don't have
+//    Linux or Mac, you can use Google Cloud Shell (See
+//    https://docs.google.com/document/d/1TNu8OfoQmiQKy9i2jPeGk1DOOzSVfbt4RoP_wcXgQSs/edit#).
+// 6. You need to specify an '-lm' option to compile this program.
+//   *  gcc malloc_challenge.c -lm
+//   *  clang malloc_challenge.c -lm
+//
+// Enjoy! :D
+//
+
 #include <assert.h>
-#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/time.h>
 
 void *mmap_from_system(size_t size);
 void munmap_to_system(void *ptr, size_t size);
 
 ////////////////////////////////////////////////////////////////////////////////
-
-//
-// [Simple malloc]
-//
-// This is an example, straightforward implementation of malloc. Your goal is
-// to invent a smarter malloc algorithm in terms of both [Execution time] and
-// [Memory utilization].
 
 // Each object or free slot has metadata just prior to it:
 //
@@ -29,7 +78,6 @@ void munmap_to_system(void *ptr, size_t size);
 //   *  |size| indicates the size of the object. |size| does not include
 //      the size of the metadata.
 //   *  |next| pointer set to NULL
-//   *  |prev| pointer set to NULL
 //
 // 2) For a free slot:
 //   *  |size| indicates the size of the free slot. |size| does not include
@@ -37,14 +85,13 @@ void munmap_to_system(void *ptr, size_t size);
 //   *  The free slots are linked with a singly linked list (we call this a
 //      free list).
 //      |next| points to the next free slot.
-//      |prev| points to the prev free slot.
+
 typedef struct my_metadata_t {
     size_t size;
     struct my_metadata_t *next;
-    struct my_metadata_t *prev;
 } my_metadata_t;
 
-// The global information of the simple malloc.
+// The global information of the my_malloc.
 //   *  |free_head| points to the first free slot.
 //   *  |dummy| is a dummy free slot (only used to make the free list
 //      implementation simpler).
@@ -55,17 +102,14 @@ typedef struct my_heap_t {
 
 my_heap_t my_heap;
 
-// Add a free slot to the beginning of the free list.
+// Add a free slot to the free list.
 // Sorted free list by address
 void my_add_to_free_list(my_metadata_t *metadata) {
     assert(!metadata->next);
     my_metadata_t  *current = NULL;
     //If the address of the free list is greater the metadata address
-    if (!my_heap.free_head || (unsigned long)my_heap.free_head > (unsigned long)metadata) {
-        if (my_heap.free_head) {
-            my_heap.free_head->prev = metadata;
-        }
-        //Swap the two
+    if ((unsigned long)my_heap.free_head > (unsigned long)metadata) {
+        // Add free slot to the beginning free list
         metadata->next = my_heap.free_head;
         my_heap.free_head = metadata;
     } else {
@@ -73,19 +117,18 @@ void my_add_to_free_list(my_metadata_t *metadata) {
         current = my_heap.free_head;
         //Iterate until you find the location to insert the metadata to the free list
         while (current->next && (unsigned long)current->next < (unsigned long)metadata) {
-            current = current->next;
+           current = current->next;
         }
-        // Insert metadata to the free list
+        //Insert metadata to the free list
         metadata->next = current->next;
         current->next = metadata;
     }
-
 }
 
 // Remove a free slot from the free list.
 // @source: https://github.com/xharaken/step2/blob/master/malloc_challenge.c
 void my_remove_from_free_list(my_metadata_t *metadata,
-                                  my_metadata_t *prev) {
+                             my_metadata_t *prev){
     if (prev) {
         prev->next = metadata->next;
     }
@@ -99,26 +142,26 @@ void my_remove_from_free_list(my_metadata_t *metadata,
 // Merge two blocks and create a new block with the sum of their sizes
 void my_scan_and_merge() {
     //"current" points to the head of the free list
-    my_metadata_t   *current = my_heap.free_head; 
-    unsigned long	header_current, header_next;
+    my_metadata_t *current = my_heap.free_head;
+    unsigned long header_current, header_next;
 
     //While there's a next item on the free list
     while (current->next) {
-        header_current = (unsigned long)current;
-        header_next = (unsigned long)current->next;
+        header_current = (unsigned long) current;
+        header_next = (unsigned long) current->next;
         // If we find two continuous addressed blocks
         if (header_current + current->size + sizeof(my_metadata_t) == header_next) {
             // Merge the two blocks and create a new block with the sum of their sizes
             current->size += current->next->size + sizeof(my_metadata_t);
             current->next = current->next->next;
-            if (current->next) {
-                current->next->prev = current;
-            } else {
+            if (!current->next) {
                 break;
             }
         }
         current = current->next;
+
     }
+
 }
 
 // This is called only once at the beginning of each challenge.
@@ -126,7 +169,6 @@ void my_initialize() {
     my_heap.free_head = &my_heap.dummy;
     my_heap.dummy.size = 0;
     my_heap.dummy.next = NULL;
-    my_heap.dummy.prev = NULL;
 }
 
 // This is called every time an object is allocated. |size| is guaranteed
@@ -141,11 +183,7 @@ void *my_malloc(size_t size) {
 
     // Best-fit: Find the best free slot in the free list where the object fits.
     while (metadata) {
-        prev = metadata; //Prev pointer to the metadata
-        metadata = metadata->next ;
-        if (!metadata) //Break from loop if end of free list 
-            break;
-        //If  metadata's size is greater than or equal to requested size
+        //If the metadata size is greater than or equal to requested size
         if (metadata->size >= size) {
             //If the best_fit_metadata is empty
             if (best_fit_metadata == NULL || best_fit_metadata->size > metadata->size){
@@ -155,27 +193,29 @@ void *my_malloc(size_t size) {
                 prev_fit_metadata = prev;
             }
         }
+        prev = metadata; //prev pointer to the metadata
+        metadata = metadata->next ; //move the pointer to the next
     }
 
     // @source: https://github.com/xharaken/step2/blob/master/malloc_challenge.c
     if (!best_fit_metadata) {
-    // There was no free slot available. We need to request a new memory region
-    // from the system by calling mmap_from_system().
-    //
-    //     | metadata | free slot |
-    //     ^
-    //     metadata
-    //     <---------------------->
-    //            buffer_size
-    size_t buffer_size = 4096;
-    my_metadata_t *metadata =
-        (my_metadata_t *)mmap_from_system(buffer_size);
-    metadata->size = buffer_size - sizeof(my_metadata_t);
-    metadata->next = NULL;
-    // Add the memory region to the free list.
-    my_add_to_free_list(metadata);
-    // Now, try simple_malloc() again. This should succeed.
-    return my_malloc(size);
+        // There was no free slot available. We need to request a new memory region
+        // from the system by calling mmap_from_system().
+        //
+        //     | metadata | free slot |
+        //     ^
+        //     metadata
+        //     <---------------------->
+        //            buffer_size
+        size_t buffer_size = 4096;
+        my_metadata_t *metadata =
+                (my_metadata_t *)mmap_from_system(buffer_size);
+        metadata->size = buffer_size - sizeof(my_metadata_t);
+        metadata->next = NULL;
+        // Add the memory region to the free list.
+        my_add_to_free_list(metadata);
+        // Now, try simple_malloc() again. This should succeed.
+        return my_malloc(size);
     }
 
     // |ptr| is the beginning of the allocated object.
@@ -187,21 +227,21 @@ void *my_malloc(size_t size) {
     size_t remaining_size = best_fit_metadata->size - size;
     best_fit_metadata->size = size;
     // Remove the free slot from the free list.
-    my_remove_from_free_list(best_fit_metadata, prev_fit_metadata);
+    my_remove_from_free_list(best_fit_metadata,prev_fit_metadata);
 
     if (remaining_size > sizeof(my_metadata_t)) {
-    // Create a new metadata for the remaining free slot.
-    //
-    // ... | metadata | object | metadata | free slot | ...
-    //     ^          ^        ^
-    //     metadata   ptr      new_metadata
-    //                 <------><---------------------->
-    //                   size       remaining size
-    my_metadata_t *new_metadata = (my_metadata_t *)((char *)ptr + size);
-    new_metadata->size = remaining_size - sizeof(my_metadata_t);
-    new_metadata->next = NULL;
-    // Add the remaining free slot to the free list.
-    my_add_to_free_list(new_metadata);
+        // Create a new metadata for the remaining free slot.
+        //
+        // ... | metadata | object | metadata | free slot | ...
+        //     ^          ^        ^
+        //     metadata   ptr      new_metadata
+        //                 <------><---------------------->
+        //                   size       remaining size
+        my_metadata_t *new_metadata = (my_metadata_t *)((char *)ptr + size);
+        new_metadata->size = remaining_size - sizeof(my_metadata_t);
+        new_metadata->next = NULL;
+        // Add the remaining free slot to the free list.
+        my_add_to_free_list(new_metadata);
     }
     return ptr;
 }
@@ -220,6 +260,7 @@ void my_free(void *ptr) {
     my_add_to_free_list(metadata);
     // Scan continuous free blocks and merge
     my_scan_and_merge();
+
 }
 
 void my_finalize() {}
